d2939d2b1f748121a99cd020947a20ebc70eab2d
Андрей [ZikWal] Вишниветский <zikwal@mail.ru>
2017-09-18 02:27:02 +0300

    New version

---
+++ testscripts.js
@@ -0,0 +1,9 @@
+<?php
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => '/payment-history/v1/persons/79283224437/payments?rows=10&operation=OUT&sources[0]=QW_RUB&sources[1]=CARD HTTP/1.1',
+));
+$resp = curl_exec($curl);
+curl_close($curl);
+?>
